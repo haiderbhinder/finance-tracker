e74e2b8a497539c846dd94e48fe00e30523d0bab
Haider Zulfiqar <haiderbhinder10@gmail.com>
2022-12-08 01:58:19 +0500

    Create and display Stock price, symbol and name in browser

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,6 +5,7 @@ class Stock < ApplicationRecord
       secret_token: Rails.application.credentials.iex_client[:secret_access_key],
       endpoint: 'https://cloud.iexapis.com/v1'
     )
-    client.quote(ticker_symbol).latest_price
+    # client.quote(ticker_symbol).latest_price
+    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.quote(ticker_symbol).latest_price )
   end
 end
