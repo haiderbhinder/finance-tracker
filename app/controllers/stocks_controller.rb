59cefae594e9d382d377c7a7341d206a449711ed
Haider Zulfiqar <haiderbhinder10@gmail.com>
2022-12-08 21:30:45 +0500

    Form submission by Ajax and responses for invalid results

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,24 @@
 class StocksController < ApplicationController
+  
   def search
-    @stock = Stock.new_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Enter a valid symbol"
+          format.js { render partial: 'users/result' }
+        end  
+      end    
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Enter a symbol"
+        format.js { render partial: 'users/result' }
+      end  
+    end   
   end
+
 end
